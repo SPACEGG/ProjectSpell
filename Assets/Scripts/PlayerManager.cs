2eaa8466c72ebae7cb250414b8be359c0c2569a6
SPACEGG <cny2410@naver.com>
2025-05-09 17:36:03 +0900

    refactor: PlayerManager 두개 겹치던거 적절히 합침

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,21 +1,17 @@
 using Cysharp.Threading.Tasks;
 using UnityEngine;
-<<<<<<< HEAD
-using Spell.Model.Core; // 추가
+using Spell.Model.Core;
 using Common.Models;
 using Common.Data;
-=======
-using Spell.Model.Core;
 using Spell.Model.Data;
 using Spell.Model.Enums;
->>>>>>> feat/projectile-behavior
 
 public class PlayerManager : MonoBehaviour
 {
     public KeyCode attackKey = KeyCode.Mouse0;
     public KeyCode recordKey = KeyCode.Mouse1;
     public UIController uiController;
-    public SpellData defaultSpell;      // TODO: 이거는 나중에 ScriptableObject로 할 것
+    public SpellData defaultSpell;
 
     [SerializeField] private HealthData healthData;
     public HealthModel HealthModel;
@@ -34,19 +30,16 @@ public class PlayerManager : MonoBehaviour
     public bool hasGravity = false;
 
     private VoiceRecorder voiceRecorder;
-    private SpellDataController spellController;
+    private SpellDataController _spellController;
     private SpellCaster spellCaster;
 
     private void Start()
     {
         voiceRecorder = new();
-<<<<<<< HEAD
         _spellController = new SpellDataController();
 
         HealthModel = new HealthModel(healthData);
         ManaModel = new ManaModel(manaData);
-=======
-        spellController = new SpellDataController();
         spellCaster = GetComponent<SpellCaster>();
 
         defaultSpell = SpellDataFactory.Create(
@@ -63,7 +56,6 @@ public class PlayerManager : MonoBehaviour
             speed,
             5f
         );
->>>>>>> feat/projectile-behavior
     }
 
     private void Update()
@@ -96,26 +88,13 @@ public class PlayerManager : MonoBehaviour
 
     private async UniTaskVoid UseSpell()
     {
-<<<<<<< HEAD
-        // spell 생성 (이게 시간이 좀 걸림)
+        // spell 생성
         await _spellController.BuildSpellDataAsync(
             voiceRecorder.VoiceClip,
             1,
             Camera.main != null ? Camera.main.transform.position : Vector3.zero,
-            this.transform.position // 캐스터 위치 추가
+            transform.position // 캐스터 위치 추가
         ); // powerLevel 기본값 1, cameraTargetPosition, casterPosition 추가
         // TODO: spell 생성 후 바로 skill을 실행할 것인가? 아니면 skillReady 플래그 같은걸 두는가?
     } // yield return StartCoroutine(스킬실행);
-} // 클래스 종료
-=======
-        SpellData spellData = await spellController.BuildSpellDataAsync(
-            voiceRecorder.VoiceClip,
-            1,
-            Camera.main != null ? Camera.main.transform.position : Vector3.zero,
-            transform.position
-        );
-
-        spellCaster.CastSpell(spellData);
-    }
 }
->>>>>>> feat/projectile-behavior
