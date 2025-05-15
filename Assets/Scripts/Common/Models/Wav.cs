6273623809c4693723c5e6f56b4fa3d357a31516
ruh0n <wns8256@naver.com>
2025-05-15 20:02:06 +0900

    feat(multiplay): Wav 구조체에 네트워크 직렬화 기능 추가

--- Assets/Scripts/Common/Models/Wav.cs
+++ Assets/Scripts/Common/Models/Wav.cs
@@ -1,12 +1,27 @@
-﻿namespace Common.Models
+﻿using Unity.Netcode;
+
+namespace Common.Models
 {
-    public readonly struct Wav
+    public struct Wav : INetworkSerializable
     {
         public byte[] Value { get; }
 
         public Wav(byte[] value)
         {
             Value = value;
+        }
+
+        public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+        {
+            if (serializer.IsWriter)
+            {
+                serializer.GetFastBufferWriter().WriteValueSafe(Value);
+            }
+            else
+            {
+                serializer.GetFastBufferReader().ReadValueSafe(out byte[] value);
+                this = new Wav(value);
+            }
         }
     }
 }
