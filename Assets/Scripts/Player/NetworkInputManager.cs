471d96cd0bf95bdd8b4eb217b08d9f31830572aa
ruh0n <wns8256@naver.com>
2025-05-15 15:25:33 +0900

    feat: 플레이어 애니메이션 동기화

---
+++ Assets/Scripts/Player/NetworkInputManager.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using Record;
+using Spell.Model.Core;
+using Spell.Model.Data;
+using Spell.Model.Enums;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace Player
+{
+    public class NetworkInputManager : NetworkBehaviour
+    {
+        [SerializeField]
+        private KeyCode attackKey = KeyCode.Mouse0;
+        [SerializeField]
+        private KeyCode recordKey = KeyCode.Mouse1;
+        [SerializeField]
+        private float recordIgnoreDuration = 0.5f;
+
+        private SpellData _defaultSpell;
+        private float _recordStartTime;
+
+        private RecordController _recordController;
+        private SpellDataController _spellController;
+        private NetworkSpellCaster _spellCaster;
+
+        private void Awake()
+        {
+            _recordController = new();
+            _spellController = new();
+        }
+
+        public override void OnNetworkSpawn()
+        {
+            _spellCaster = GetComponent<NetworkSpellCaster>();
+
+            _defaultSpell = SpellDataFactory.Create(
+                "Default Spell",
+                ElementType.Earth,
+                BehaviorType.Projectile,
+                new List<SpellActionData>() { new(ActionType.Damage, TargetType.Enemy, 10) },
+                Vector3.zero,
+                Vector3.forward,
+                1,
+                ShapeType.Sphere,
+                Vector3.one,
+                true,
+                10f,
+                5f
+            );
+        }
+
+        private void Update()
+        {
+            if (!IsLocalPlayer) return;
+
+            if (Input.GetKeyDown(attackKey))
+            {
+                _spellCaster.CastSpellRpc(_defaultSpell);
+            }
+
+            if (Input.GetKeyDown(recordKey))
+            {
+                _recordController.StartRecording();
+                // uiController.ShowRecordIcon();
+                _recordStartTime = Time.time;
+            }
+
+            if (Input.GetKeyUp(recordKey))
+            {
+                _recordController.StopRecording();
+                // uiController.HideRecordIcon();
+
+                if (Time.time - _recordStartTime >= recordIgnoreDuration)
+                {
+                    // UseSpellServerRpc().Forget();
+                }
+            }
+        }
+
+        // TODO: 서버에서 스펠을 사용하는 로직 추가
+        // Spell 사용
+        // [ServerRpc]
+        // private async UniTaskVoid UseSpellServerRpc()
+        // {
+        //     var spellData = await _spellController.BuildSpellDataAsync(
+        //         _recordController.GetRecordingClip(),
+        //         1,
+        //         Camera.main ? Camera.main.transform.position : Vector3.zero,
+        //         transform.position
+        //     );
+        //
+        //     // TODO: 마나 소모
+        //     // healthManaManager.ManaModel.UseMana(200f);
+        //     _spellCaster.CastSpell(spellData);
+        // }
+    }
+}
---
+++ Assets/Scripts/Spell/Model/Core/NetworkSpellCaster.cs
@@ -0,0 +1,33 @@
+﻿using Spell.Model.Data;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace Spell.Model.Core
+{
+    public class NetworkSpellCaster : NetworkBehaviour
+    {
+        [SerializeField] private Transform castOrigin; // 지팡이 끝부분
+
+        public Transform CastOrigin => castOrigin ? castOrigin : Camera.main?.transform; // 여기서 transform 사용
+
+        [Rpc(SendTo.Server)]
+        public void CastSpellRpc(SpellData data)
+        {
+            if (data == null)
+            {
+                Debug.LogWarning("Tried to cast null spell.");
+                return;
+            }
+
+            // SpellData에서 PositionOffset을 가져와서 사용 (null이면 2칸 앞)
+            var offset = data.PositionOffset ?? new Vector3(0f, 0f, 2f);
+            var spawnPosition = CastOrigin.position + offset;
+
+            // SpellData 정보를 바탕으로 실제 동작/외형이 적용된 스펠 오브젝트 씬에 생성
+            var spellBehavior = SpellFactory.CreateSpellGameObject(data);
+            spellBehavior.transform.SetPositionAndRotation(spawnPosition, CastOrigin.rotation);
+            spellBehavior.Behave(data);
+            // 이 이후에 spellBehavior의 Start()가 실행됨
+        }
+    }
+}
